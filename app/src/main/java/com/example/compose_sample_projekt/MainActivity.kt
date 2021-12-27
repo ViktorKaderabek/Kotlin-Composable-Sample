e9f5a6def8eddf5cd8a0d2cf45dcae904114041d
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2021-12-27 18:55:24 +0100

    Projekt

--- app/src/main/java/com/example/compose_sample_projekt/MainActivity.kt
+++ app/src/main/java/com/example/compose_sample_projekt/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.compose_sample_projekt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -19,6 +19,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
@@ -211,6 +213,48 @@ class MainActivity : ComponentActivity() {
 //---------------------------------------------------------------------------------------------\\
 
 
+       /*     val scrollState = rememberScrollState()
+            Column(modifier = Modifier.verticalScroll(scrollState)) {
+
+                for (i in 1..50) {
+                    Text(
+                        text = "Item $i",
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp)
+                    )
+
+                }
+
+            }
+
+        */
+//---------------------------------------------------------------------------------------------\\
+
+/*
+            LazyColumn{
+                itemsIndexed(
+                    listOf("This","is","Jetpack","Compose")
+                ){index,string ->
+
+                   Text(
+                       text = string,
+                       fontSize = 24.sp,
+                       textAlign = TextAlign.Center,
+                       fontWeight = FontWeight.Bold,
+                       modifier = Modifier
+                           .fillMaxWidth()
+                           .padding(vertical = 24.dp)
+                   )
+               }
+
+            }
+
+ */
+//---------------------------------------------------------------------------------------------\\
 
 
         }
