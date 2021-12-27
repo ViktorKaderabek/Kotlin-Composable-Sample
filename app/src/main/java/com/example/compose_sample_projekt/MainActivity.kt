2becfcf441de2b7d2cc6c0405af7492f55971fa3
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2021-12-27 15:17:52 +0100

    Projekt

--- app/src/main/java/com/example/compose_sample_projekt/MainActivity.kt
+++ app/src/main/java/com/example/compose_sample_projekt/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.compose_sample_projekt
 
+import android.graphics.fonts.Font
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -19,7 +20,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,8 +34,9 @@ import com.example.compose_sample_projekt.ui.theme.Compose_sample_projektTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            //------------------------------------------------------\\
+//---------------------------------------------------------------------------------------------\\
             /*  Column(
                   modifier = Modifier
                       .fillMaxWidth()
@@ -55,7 +62,7 @@ class MainActivity : ComponentActivity() {
                   Text("Hello")
                   Text("World")
               }*/
-//------------------------------------------------------\\
+//---------------------------------------------------------------------------------------------\\
             /*   Column(
                    modifier = Modifier
                        .background(Color.Cyan)
@@ -83,21 +90,61 @@ class MainActivity : ComponentActivity() {
                }
 
             */
-//------------------------------------------------------\\
+//---------------------------------------------------------------------------------------------\\
 
-            val painter = painterResource(id = R.drawable.kermit)
-            val description = "Kermit in the snow"
-            val title = "Kermit palying in the snow"
+            /*    val painter = painterResource(id = R.drawable.kermit)
+                  val description = "Kermit in the snow"
+                  val title = "Kermit palying in the snow"
 
-            Box(
+                  Box(
+                      modifier = Modifier
+                          .fillMaxWidth(0.5f)
+                          .fillMaxHeight(0.5f)
+                          .padding(10.dp)
+                  ) {
+
+                      ImageCard(painter = painter, contentDescription = description, title = title)
+                  }
+
+             */
+//---------------------------------------------------------------------------------------------\\
+
+        /*    Box(
                 modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .fillMaxHeight(0.5f)
-                    .padding(10.dp)
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
             ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 30.sp
+                            )
+                        ){
+                            append("J")
+                        }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 30.sp
+                            )
+                        ){
+                            append("C")
+                        }
+                        append("ompose ")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    textDecoration = TextDecoration.Underline
+                )
 
-                ImageCard(painter = painter, contentDescription = description, title = title)
             }
+
+         */
+//---------------------------------------------------------------------------------------------\\
+
         }
     }
 }
