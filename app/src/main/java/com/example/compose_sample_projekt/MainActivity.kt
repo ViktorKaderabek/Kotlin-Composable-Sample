111e85efb343fd5c58df4d79f3dbc96ca583d2bc
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2021-12-27 17:06:14 +0100

    Projekt

--- app/src/main/java/com/example/compose_sample_projekt/MainActivity.kt
+++ app/src/main/java/com/example/compose_sample_projekt/MainActivity.kt
@@ -1,35 +1,30 @@
 package com.example.compose_sample_projekt
 
-import android.graphics.fonts.Font
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.compose_sample_projekt.ui.theme.Compose_sample_projektTheme
+import kotlin.random.Random
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -109,41 +104,65 @@ class MainActivity : ComponentActivity() {
              */
 //---------------------------------------------------------------------------------------------\\
 
-        /*    Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFF101010))
-            ) {
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 30.sp
-                            )
-                        ){
-                            append("J")
-                        }
-                        append("etpack ")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 30.sp
-                            )
-                        ){
-                            append("C")
-                        }
-                        append("ompose ")
-                    },
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    textDecoration = TextDecoration.Underline
+            /*    Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFF101010))
+                ) {
+                    Text(
+                        text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Green,
+                                    fontSize = 30.sp
+                                )
+                            ){
+                                append("J")
+                            }
+                            append("etpack ")
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Green,
+                                    fontSize = 30.sp
+                                )
+                            ){
+                                append("C")
+                            }
+                            append("ompose ")
+                        },
+                        color = Color.White,
+                        fontSize = 30.sp,
+                        textDecoration = TextDecoration.Underline
+                    )
+
+                }
+
+             */
+//---------------------------------------------------------------------------------------------\\
+
+            Column(Modifier.fillMaxSize()) {
+
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+                    color.value = it
+                }
+
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
                 )
 
             }
 
-         */
-//---------------------------------------------------------------------------------------------\\
 
         }
     }
@@ -196,6 +215,19 @@ fun ImageCard(
 
         }
     }
+}
 
+
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit
+) {
+
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f))
+        })
 
 }
