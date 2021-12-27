a877ae7758de01d67cc3c2b48e3a56faa9344e82
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2021-12-27 19:28:13 +0100

    Projekt

--- app/src/main/java/com/example/compose_sample_projekt/MainActivity.kt
+++ app/src/main/java/com/example/compose_sample_projekt/MainActivity.kt
@@ -18,11 +18,17 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
@@ -213,25 +219,25 @@ class MainActivity : ComponentActivity() {
 //---------------------------------------------------------------------------------------------\\
 
 
-       /*     val scrollState = rememberScrollState()
-            Column(modifier = Modifier.verticalScroll(scrollState)) {
+            /*     val scrollState = rememberScrollState()
+                 Column(modifier = Modifier.verticalScroll(scrollState)) {
 
-                for (i in 1..50) {
-                    Text(
-                        text = "Item $i",
-                        fontSize = 24.sp,
-                        textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 24.dp)
-                    )
+                     for (i in 1..50) {
+                         Text(
+                             text = "Item $i",
+                             fontSize = 24.sp,
+                             textAlign = TextAlign.Center,
+                             fontWeight = FontWeight.Bold,
+                             modifier = Modifier
+                                 .fillMaxWidth()
+                                 .padding(vertical = 24.dp)
+                         )
 
-                }
+                     }
 
-            }
+                 }
 
-        */
+             */
 //---------------------------------------------------------------------------------------------\\
 
 /*
@@ -255,8 +261,39 @@ class MainActivity : ComponentActivity() {
 
  */
 //---------------------------------------------------------------------------------------------\\
+            /*
+            val constraints = ConstraintSet {
+                val greenBox = createRefFor("greenbox")
+                val redBox = createRefFor("redbox")
+                val guideline = createGuidelineFromTop(0.5f)
 
+                constrain(greenBox) {
+                    top.linkTo(guideline)
+                    start.linkTo(parent.start)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+                constrain(redBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    end.linkTo(parent.end)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
 
+                createHorizontalChain(redBox,greenBox, chainStyle = ChainStyle.Packed)
+            }
+            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+
+                Box(modifier = Modifier.background(Color.Green)
+                    .layoutId("greenbox"))
+
+                Box(modifier = Modifier.background(Color.Red)
+                    .layoutId("redbox"))
+            }
+
+             */
+//---------------------------------------------------------------------------------------------\\
         }
     }
 }
